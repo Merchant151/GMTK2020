ee0f4693cd5f64d374664453091508c897f58ab7
Mark-Stiller <46461833+Mark-Stiller@users.noreply.github.com>
2020-07-11 11:22:44 -0400

    made map prefabs, updated camera controls

--- GMTK2020/Assets/CameraController.cs
+++ GMTK2020/Assets/CameraController.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     float h, v, movespeed;
+    //float mx, my, lookspeed;
+    Vector2 mz;
+    float zoomspeed;
 
     // Start is called before the first frame update
     void Start()
     {
         h = v = 0;
         movespeed = 10;
+        /*mx = my = 0;
+        lookspeed = 5;*/
+        transform.Rotate(new Vector3(45, -45, 0), Space.World); //40, -30 also works
+        mz = Vector2.zero;
+        zoomspeed = 0.7f;
     }
 
     // Update is called once per frame
@@ -18,9 +26,20 @@ public class CameraController : MonoBehaviour
     {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
-
-        Vector3 step = new Vector3(h, v, 0);
-
+        Vector3 step = new Vector3(h, 0, v);
+        movespeed = 2.5f * transform.position.y;
         transform.position += step * Time.deltaTime * movespeed;
+
+        mz = Input.mouseScrollDelta * zoomspeed;
+        Vector2 zoomstep = transform.position - (Vector3)mz;
+        if(zoomstep.y >= 5 && zoomstep.y <= 12)
+        {
+            transform.position -= (Vector3)mz;
+        }
+
+        /*mx = Input.GetAxis("Mouse X");
+        my = Input.GetAxis("Mouse Y");
+        Vector3 look = new Vector3(-my, mx, 0);
+        transform.Rotate(look * lookspeed, Space.Self);*/
     }
 }
