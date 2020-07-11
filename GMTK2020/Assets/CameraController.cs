2aa8f2cc7bc41ff92dc5947c866c7b85e98ed3a0
Mark-Stiller <46461833+Mark-Stiller@users.noreply.github.com>
2020-07-11 00:59:56 -0400

    basic 3D map completed, tags added

---
+++ GMTK2020/Assets/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    float h, v, movespeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        h = v = 0;
+        movespeed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 step = new Vector3(h, v, 0);
+
+        transform.position += step * Time.deltaTime * movespeed;
+    }
+}
