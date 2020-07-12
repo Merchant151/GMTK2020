2bb2a584625ed3f2a80e10ab8bb1b710526f6b4a
Mark-Stiller <46461833+Mark-Stiller@users.noreply.github.com>
2020-07-12 01:34:03 -0400

    enemy spawns, despawning on hitting player hp

---
+++ GMTK2020/Assets/EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    GameObject[] goals;
+    float movespeed;
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goals = GameObject.FindGameObjectsWithTag("PlayerHP");
+        int aim = Random.Range(0, goals.Length - 1);
+        target = goals[aim];
+        transform.LookAt(target.transform);
+        movespeed = 0.02f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * movespeed;
+
+        Collider[] inrange = Physics.OverlapSphere(transform.position, 0.2f);
+        foreach(Collider checking in inrange)
+        {
+            if (checking.tag == "PlayerHP") Destroy(gameObject);
+        }
+        /*Vector3 step = transform.forward * movespeed;
+        if (transform.position + step == target.transform.position)
+        {
+
+        }*/
+    }
+}
