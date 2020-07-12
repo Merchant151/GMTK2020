d4577084cfd085e4d6cf585c37b8abb4a3f2a369
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2020-07-12 01:06:14 -0400

    basic AI

---
+++ GMTK2020/Assets/WorldGrid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGrid : MonoBehaviour
+{
+    public int length;
+    public int width;
+    public float size;
+
+    private int[,] grid;
+
+    public WorldGrid()
+    {
+        length = 4;
+        width = 4;
+        size = 10;
+
+        grid = new int[length, width];
+    }
+
+
+
+
+
+}
---
+++ GMTK2020/Assets/bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    private Vector3 direction;
+    public float speed;
+
+    private GameObject target;
+    
+    public void Setup(Vector3 direction, GameObject target)
+    {
+        this.direction = direction;
+        Destroy(gameObject, 2f);
+        this.target = target;
+    }       
+
+    // Update is called once per frame
+    void Update()
+    {
+        float range = 2f;
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(gameObject.transform.position , target.transform.position) < range)
+        {
+            Destroy(gameObject);
+        } 
+    }
+}
