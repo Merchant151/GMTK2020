5f2c2278c581c13ac52330c9c5a404db2cb2e2b8
Mark-Stiller <46461833+Mark-Stiller@users.noreply.github.com>
2020-07-11 23:34:39 -0400

    tower placement beta, need to extend

--- GMTK2020/Assets/CameraController.cs
+++ GMTK2020/Assets/CameraController.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     float h, v, movespeed;
-    //float mx, my, lookspeed;
+    float mx, my, lookspeed;
     Vector2 mz;
     float zoomspeed;
+
+    public GameObject testTower;
+    public GameObject spawnpoint;
+    GameObject activeTower;
+    Vector3 mousePos;
+    bool placed;
 
     // Start is called before the first frame update
     void Start()
     {
         h = v = 0;
         movespeed = 10;
-        /*mx = my = 0;
-        lookspeed = 5;*/
+        mx = my = 0;
+        lookspeed = 5;
         transform.Rotate(new Vector3(45, -45, 0), Space.World); //40, -30 also works
         mz = Vector2.zero;
         zoomspeed = 0.7f;
+        placed = true;
     }
 
     // Update is called once per frame
@@ -28,13 +35,30 @@ public class CameraController : MonoBehaviour
         v = Input.GetAxis("Vertical");
         Vector3 step = new Vector3(h, 0, v);
         movespeed = 2.5f * transform.position.y;
-        transform.position += step * Time.deltaTime * movespeed;
+        if (placed)
+        {
+            transform.position += step * Time.deltaTime * movespeed;
+        }
 
         mz = Input.mouseScrollDelta * zoomspeed;
         Vector2 zoomstep = transform.position - (Vector3)mz;
         if(zoomstep.y >= 5 && zoomstep.y <= 12)
         {
             transform.position -= (Vector3)mz;
+        }
+
+        if (Input.GetButtonDown("Fire1") && placed)
+        {
+            activeTower = Instantiate(testTower, spawnpoint.transform);
+            placed = false;
+        }
+        if (!placed)
+        {
+            activeTower.transform.position += step * Time.deltaTime * movespeed;
+            if (Input.GetButtonDown("Fire2"))
+            {
+                placed = true;
+            }
         }
 
         /*mx = Input.GetAxis("Mouse X");
