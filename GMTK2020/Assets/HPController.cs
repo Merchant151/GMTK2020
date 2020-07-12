aa9c96750c4fc00bb63f8a9c25268e83075ff0e8
Mark-Stiller <46461833+Mark-Stiller@users.noreply.github.com>
2020-07-12 12:41:09 -0400

    need to add HPandScore script to enemy prefab

--- GMTK2020/Assets/HPController.cs
+++ GMTK2020/Assets/HPController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class HPController : MonoBehaviour
 {
+    public int hp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = 10;
     }
 
     // Update is called once per frame
@@ -16,8 +18,8 @@ public class HPController : MonoBehaviour
         
     }
 
-    private void OnCollisionStay(Collision collision)
+    /*private void OnCollisionStay(Collision collision)
     {
         Destroy(collision.gameObject);
-    }
+    }*/
 }
